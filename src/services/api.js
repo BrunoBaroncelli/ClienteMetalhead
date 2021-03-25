e0c0677a2e5bc80019614ba111d037b892df8721
Bruno Baroncelli <brunolbaroncelli@gmail.com>
2021-03-25 16:23:03 -0300

    link do heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://api-metalhead.herokuapp.com",
 });
 const token = localStorage.getItem("token");
 if (token) {
